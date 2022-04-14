8c6799b6b9c28d789fe283c769ed8cbbfce31ce3
Thiago F <tcfialho@gmail.com>
2022-04-14 14:18:12 -0300

    feat: initial

---
+++ Herald.MediatR.Retry.Tests/RetryPolicyBehaviorTests.cs
@@ -0,0 +1,53 @@
+﻿using Activities.Application.Infrastructure.RetryPolicy;
+using Activities.Infrastructure;
+
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Herald.MediatR.Retry.Tests
+{
+    [RetryPolicy]
+    public class Command : IRequest<bool>
+    {
+    }
+
+    public class Handler : IRequestHandler<Command, bool>
+    {
+        public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+        {
+            throw new ArgumentException();
+            return await Task.FromResult(true);
+        }
+    }
+
+    public class RetryPolicyBehaviorTests
+    {
+        [Fact]
+        public async Task ShouldThrowsApplicationExceptionWhenReachMaxRetryCount()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+
+            services.AddLogging();
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RetryPolicyBehavior<,>));
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            var mediator = services.BuildServiceProvider().GetService<IMediator>();
+            var cmd = new Command();
+
+            //Act
+            Func<Task> act = () => mediator.Send(cmd);
+
+            //Assert
+            Assert.ThrowsAsync<ApplicationException>(act);
+        }
+    }
+}
